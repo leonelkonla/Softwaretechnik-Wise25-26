7925547697b0de142470764204b067d676a6efcb
Leo Ogger <239398730+leog333@users.noreply.github.com>
2025-12-03 18:41:04 +0100

    LoginUI + Authentication Controller + DataBase Imitation

--- src/main/java/data/DataBaseImit.java
+++ src/main/java/data/DataBaseImit.java
@@ -1,3 +1,8 @@
+/*
+Klasse, die unsere noch nicht vorhandene
+Datenbank imitieren soll.
+ */
+
 package data;
 import model.*;
 import java.util.ArrayList;
@@ -6,6 +11,8 @@ import java.util.List;
 public class DataBaseImit {
 
     private List<User> users = new ArrayList<>();
+
+    //Um in Main erstellte DataBase für AuthController verfügbar zu machen
     public static DataBaseImit instance;
 
     public DataBaseImit() {
